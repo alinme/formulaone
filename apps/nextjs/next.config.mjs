399be7aaaf28a316b0d6eeb108e354500d30f401
ALIN <dev@ALINs-MacBook-Pro.local>
2026-02-11 01:32:56 +0200

    Netlify fix attempt #1

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -20,12 +20,13 @@ const config = {
     // serverActions: true,
   },
   images: {
+    unoptimized: true, // required for static export
     domains: ["images.unsplash.com", "avatars.githubusercontent.com", "www.twillot.com", "cdnv2.ruguoapp.com", "www.setupyourpay.com"],
   },
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  output: "standalone",
+  output: "export",
 };
 
 export default withNextDevtools(withMDX()(config));
